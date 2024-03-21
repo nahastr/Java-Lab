037b5aeee2297e4795fa462728f8b057fe4ccc3d
nahastr <70069790+nahastr@users.noreply.github.com>
2024-03-21 15:09:30 +0530

    Update SortWord.java

--- SortWord.java
+++ SortWord.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
-
-class SortWord
+class SortString
 {
 	public static void main(String args[])
 	{
-	
+		Scanner in = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		int n = 0;
+		System.out.println("Enter the number of strings : ");
+		n = in.nextInt();
+		
+		String str[] = new String[n];
+		
+		System.out.println("Enter the stings :");
+		for(int i = 0; i < n; ++i)
+			str[i] = sc.nextLine();
+			
+		Arrays.sort(str);
+		
+		System.out.println("Sorted elements are :");
+		for(int i = 0; i < n; ++i)
+			System.out.println(str[i]);
 	}
-}
+}	
