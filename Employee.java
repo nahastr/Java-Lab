b4e414816786c42134e3a3cd22d7ba8a31eaeed6
nahastr <70069790+nahastr@users.noreply.github.com>
2024-03-24 23:03:48 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,64 +1,66 @@
 import java.util.Scanner;
 
-class Details
-{
-	int eNo = 0;
-	String eName = null;
-	int eSalary = 0;
-	
-	Details(int no, String name, int salary)
-	{
-		eNo = no;
-		eName = name;
-		eSalary = salary;
-	}
-	
-	void display()
-	{
-		System.out.println("\nEmployee no : " + eNo + " Name : " + eName + " Salary : " + eSalary + "\n");
-	}
+class Details {
+    int eNo = 0;
+    String eName = null;
+    int eSalary = 0;
 
+    Details(int no, String name, int salary) {
+        eNo = no;
+        eName = name;
+        eSalary = salary;
+    }
+
+    void display() {
+        System.out.println(eNo + "      " + eName + "       " + eSalary);
+    }
 }
 
-class Employee
-{
-	public static void main(String args[])
-	{
-		Scanner in = new Scanner(System.in);
-		Scanner st = new Scanner(System.in);
-		System.out.println("Enter the number of Employees : ");
-		int n = in.nextInt();
-		
-		Details [] emp = new Details[n];
-		
-		int no = 0;
-		String name = null;
-		int salary = 0;
-		
-		for(int i = 0; i < n; ++i)
-		{
-			System.out.print("Enter the employee number : ");
-			no = in.nextInt();
-			System.out.print("Enter the employee name : ");
-			name = st.nextLine();
-			System.out.print("Enter the employee salary : ");
-			salary = in.nextInt();
-			emp[i] = new Details(no, name, salary);
-		}
-		
-		System.out.println("Enter the empolyee number to be searched : ");
-		int num = in.nextInt();
-		
-		for(int i = 0; i < n; ++i)
-		{
-			if(emp[i].eNo == num)
-				emp[num].display();
-			else
-				System.out.println("Employee dosen't exist ");
-		}
-		
-		for(int i = 0; i < n; ++i)
-			emp[i].display();
-		
-	}
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of Employees : ");
+        int n = in.nextInt();
+        in.nextLine(); // consume the newline character
+
+        Details[] emp = new Details[n];
+
+        int no = 0;
+        String name = null;
+        int salary = 0;
+
+        for (int i = 0; i < n; ++i) {
+            System.out.print("Enter the employee number : ");
+            no = in.nextInt();
+            in.nextLine(); // consume the newline character
+            System.out.print("Enter the employee name : ");
+            name = in.nextLine();
+            System.out.print("Enter the employee salary : ");
+            salary = in.nextInt();
+            emp[i] = new Details(no, name, salary);
+        }
+
+        System.out.println("Enter the employee number to be searched : ");
+        int num = in.nextInt();
+        boolean flag = false;
+        int i;
+        for (i = 0; i < n; ++i) {
+            if (emp[i].eNo == num) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag)
+            System.out.println("Employee exists:");
+        else
+            System.out.println("Employee doesn't exist.");
+
+        emp[i].display();
+
+
+        System.out.println("Employee id         Name    Salary");
+        for (i = 0; i < n; ++i)
+            emp[i].display();
+    }
 }
